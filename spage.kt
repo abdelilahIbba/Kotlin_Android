954585b52dd044912c9ac6ae70c14bc8d37493b3
abdelilahIbba <2001081800535@ofppt-edu.ma>
2024-10-07 15:40:46 +0100

    learning functions

---
+++ spage.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val str = "hello"
+    println(str)
+
+}
--- variabels.kt
+++ variabels.kt
@@ -1,13 +1,24 @@
 fun main() {
-    val c1: Int = 22
-    val c2: String = "Abdelilah"
-    val c3: Double = 22.0
-    val c4: Float = 2.0F
-    val c5: Boolean = false
+    var c1: Int = 22
+    var c2: String = "Abdelilah"
+    var c3: Double = 22.0
+    var c4: Float = 2.0F
+    var c5: Boolean = false
+
+    c1 = 19
+    c2 = "Abdelilah \"El Ibbawi\""
+    c3 = .82
+    c4 = 3.8F
+    c5 = true
 
     println("c1: $c1")        // Prints the Int variable
     println("c2: $c2")        // Prints the String variable
     println("c3: $c3")        // Prints the Double variable
     println("c4: $c4")        // Prints the Float variable
     println("c5: $c5")        // Prints the Boolean variable
+
+    /*
+     * This program displays the number of messages
+     * in the user's inbox.
+     */
 }
