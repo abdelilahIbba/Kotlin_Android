a642564026ec53118ceaa08d3da8be7d7be8c2c4
abdelilahIbba <162345757+abdelilahIbba@users.noreply.github.com>
2024-10-07 12:45:04 +0100

    Update variabels.kt

---
+++ variabels.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val c1: Int = 22
+    val c2: String = "Abdelilah"
+    val c3: Double = 22.0
+    val c4: Float = 2.0F
+    val c5: Boolean = false
+
+    println("c1: $c1")        // Prints the Int variable
+    println("c2: $c2")        // Prints the String variable
+    println("c3: $c3")        // Prints the Double variable
+    println("c4: $c4")        // Prints the Float variable
+    println("c5: $c5")        // Prints the Boolean variable
+}
